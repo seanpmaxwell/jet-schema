c96060304e5a0aa57ebb2317a9352ad0ec4714f5
seanpmaxwell <spmaxwell7@gmail.com>
2024-10-24 01:16:25 -0700

    fixed bug validating dates

--- test/index.ts
+++ test/index.ts
@@ -3,11 +3,13 @@ import User from './models/User';
 import { isNumArr } from './util/validators';
 
 
+// Test schema new
 console.log(User.new({
   lastLogin: '2023-12-25' as unknown as Date,
   age: '123' as unknown as number,
 }));
 
 
+// Test trans function
 const customTest: ReturnType<typeof trans> = trans(JSON.parse, isNumArr);
-customTest('[1,2,3,4]');
+console.log(customTest('[1,2,3,5]'));
